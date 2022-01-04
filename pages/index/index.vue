d9f96353d3e834dd036a0ff6393186ba3c2737d6
wii <dicoholicxxx@gmail.com>
2022-01-04 18:46:47 +0800

    fix: 固定游戏id

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -825,6 +825,7 @@
 			this.navbarHeight =
 				getApp().globalData.statusBarHeight + getApp().globalData.navBarHeight
 			let localGameId = this.$storage.get('gameId')
+			localGameId = '211221074055329951'
 			if (options.gameId && options.gameId !== localGameId) {
 				localGameId = options.gameId
 				this.$storage.set('gameId', options.gameId)
