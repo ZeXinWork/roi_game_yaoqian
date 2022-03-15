6a0492aaf39a774cce5f2002871db0e98d72372a
wii <dicoholicxxx@gmail.com>
2022-03-15 17:30:05 +0800

    fix：开卡组件选择关闭窗口

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1324,7 +1324,7 @@
 			},
 			closeAdOpenHelp() {
 				this.$refs.vipCardOpenHelp.close()
-				this.showNoPlayNum()
+				// this.showNoPlayNum()
 			},
 			openCard(isNotify, location) {
 				const gameInfo = this.gameInfo
@@ -1352,8 +1352,11 @@
 						console.log("跳转开卡失败: ", err)
 					},
 					complete: function() {
-						that.closeAdOpenHelp()
-						that.closeAdOpen()
+						if (location == 'help') {
+							that.closeAdOpenHelp()
+						} else {
+							that.closeAdOpen()
+						}
 					}
 				})
 			},
