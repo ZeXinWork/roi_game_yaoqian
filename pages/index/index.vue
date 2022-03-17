bd8eeba304fd3ac9d06dff138fa61a074b352c01
wii <dicoholicxxx@gmail.com>
2022-03-17 14:38:46 +0800

    fix: 修复切换游戏错误弹出开卡通知

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1946,6 +1946,7 @@
 				// this.getMyRank()
 				// //获取游戏可玩次数
 				// this.getPlayNumber(true)
+				this.getUserOpenCard()
 				this.getWechatMessage()
 				this.getGameInfo() //获取游戏信息
 				// this.getPlayNumber(true) //获取游戏可玩次数
@@ -1955,7 +1956,6 @@
 				if (this.currentScoreItem === 1) {
 					this.getAward()
 				}
-				this.getUserOpenCard()
 			},
 			// 初始化
 			getRainSetting() {
@@ -3009,11 +3009,11 @@
 						this.$uma.setOpenid(this.user.openid)
 						this.getGameInfo(() => {
 							//获取游戏信息
-							this.getPlayNumber(true) //获取游戏可玩次数
+							this.getUserOpenCard()
 							this.getHelperList(1) // 助力记录
 							this.getMyRank() //获取当前我的排名信息
+							this.getPlayNumber(true) //获取游戏可玩次数
 							this.getWechatMessage()
-							this.getUserOpenCard()
 							if (this.currentScoreItem === 1) {
 								this.getAward()
 							}
